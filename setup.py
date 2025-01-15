df1cdae7dcbd606c06e3155050cc496aad35b78f
Amit Trikha <trikhaamit@gmail.com>
2025-01-14 22:13:33 -0500

    Added files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI ChatBot',
+    version = '0.0.0',
+    author='Amit Trikha',
+    author_email='amittrikha10@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
